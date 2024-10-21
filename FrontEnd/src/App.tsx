54090f2ab971ed8d792a3a3c618d4e808b5eb805
kk <duonghuuquoc99@gmail.com>
2024-10-21 15:25:02 +0700

    base

---
+++ FrontEnd/src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+import "./App.css";
+
+import { Route, Routes } from "react-router-dom";
+import Register from "./Components/Register/Register";
+import Login from "./Components/Login/Login";
+import PrivateRouter from "./Components/PrivateRoute";
+import Dashboard from "./Admin/Dashboard";
+
+
+function App() {
+  return (
+    <>
+     
+    </>
+  );
+}
+
+export default App;
