aefbe1c1b57ba718a4a6ffde25f8d1aa7bad405e
hoadtph31026 <hoadtph31026.fpt.edu.vn>
2024-10-22 12:38:26 +0700

    Change user information

--- BackEnd/app/Http/Controllers/Client/ClientUserController.php
+++ BackEnd/app/Http/Controllers/Client/ClientUserController.php
@@ -7,5 +7,44 @@ use Illuminate\Http\Request;
 
 class ClientUserController extends Controller
 {
-    //
+    // Thay đổi thông tin người dùng
+    // public function updateUserInfo(Request $request, string $id)
+    // {
+    //     $validatedData = $request->validate([
+    //         "name" => "required|max:255",
+    //         "email" => "required|email|max:255|unique:users,email," . $id, 
+    //         "phone" => "required|max:255",
+    //         "address" => "required|max:255",
+    //         // "avatar" => "nullable|image|mimes:jpeg,png,jpg,gif|max:2048" 
+    //     ]);
+
+    //     if ($request->isMethod("PUT")) {
+    //         $param = $validatedData; 
+    //         $user = User::findOrFail($id);
+
+    //         if ($request->hasFile("avatar")) {
+    //             if ($user->avatar && Storage::disk("public")->exists($user->avatar)) 
+    //             {
+    //                 Storage::disk("public")->delete($user->avatar);
+    //             }
+                
+    //             $filepath = $request->file("avatar")->store("uploads/users", "public");
+    //             $param["avatar"] = $filepath;
+    //         } else {
+    //             $param["avatar"] = $user->avatar; 
+    //         }
+            
+    //         unset($param["password"]); 
+
+    //         $updated = $user->update($param); 
+
+    //         if ($updated) {
+    //             return response()->json(['message' => 'User updated info successfully']);
+    //         } else {
+    //             return response()->json(['message' => 'Failed to update user info'], 500);
+    //         }
+    //     }
+
+    //     return response()->json(['message' => 'Invalid request method'], 405);
+    // }
 }
