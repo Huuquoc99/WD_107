139f498c507e398ec79152b6a86852af9115c31c
hoadtph31026 <hoadtph31026.fpt.edu.vn>
2024-10-22 14:14:08 +0700

    Update Model catalogue

--- BackEnd/app/Models/Catalogue.php
+++ BackEnd/app/Models/Catalogue.php
@@ -4,8 +4,51 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Catalogue extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        "name",
+        "cover",
+        "is_active",
+    ];
+
+    protected $casts = [
+        "is_active" => "boolean",
+    ];
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
+
+    public function product()
+    {
+        return $this->hasOne(Product::class);
+    }
+
+    public function hide()
+    {
+        $this->is_active = 0;
+        $this->save();
+        
+        foreach ($this->products as $product)
+        {
+            $product->hide();
+        }
+    }
+
+    public function show()
+    {
+        $this->is_active = 1;
+        $this->save();
+
+        foreach ($this->products as $product)
+        {
+            $product->show();
+        }
+    }
 }
