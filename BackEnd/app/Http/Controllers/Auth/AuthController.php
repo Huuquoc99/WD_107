8c5b197a6500a9930da15dd92c8386c30dad90a2
hoadtph31026 <hoadtph31026.fpt.edu.vn>
2024-10-22 10:41:20 +0700

    Register

---
+++ BackEnd/app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Models\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Password;
+
+class AuthController extends Controller
+{
+    public function register()
+    {
+        try{
+            $data = request()->validate([
+                "name" => "required",
+                "email" => "required|email",
+                "password" => "required|min:8|max:20|confirmed",
+            ]);
+
+            $user = User::create($data);
+            $token = $user->createToken($user->id)->plainTextToken;
+
+            return response()->json([
+                "token" => $token
+            ]);
+        }catch(\Throwable $th){
+            if($th instanceof ValidationException){
+                return response()->json([
+                    "errors" => $th->errors()
+                ], Response::HTTP_BAD_REQUEST);
+            }
+
+            return response()->json([
+                "errors" => $th->getMessage()
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    } 
+}
