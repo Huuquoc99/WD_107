d964f1954b8d87585c1f5fa14a4380e211528e3c
hoadtph31026 <hoadtph31026.fpt.edu.vn>
2024-10-22 11:37:28 +0700

    Migration cart

--- BackEnd/database/migrations/2024_10_22_043601_create_carts_table.php
+++ BackEnd/database/migrations/2024_10_22_043601_create_carts_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use App\Models\User;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -13,11 +14,7 @@ return new class extends Migration
     {
         Schema::create('carts', function (Blueprint $table) {
             $table->id();
-            $table->string("code", 50)->unique();
-            $table->string("name", 255);
-            $table->text("description");
-            $table->integer("display_order");
-            $table->boolean("is_active")->default(1);
+            $table->foreignIdFor(model: User::class)->constrained();
             $table->timestamps();
             $table->softDeletes();
         });
