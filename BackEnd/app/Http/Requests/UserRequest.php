2e4cf775dd42f71e227b56c5e46a09af0bf2e934
hoadtph31026 <hoadtph31026.fpt.edu.vn>
2024-10-22 12:53:05 +0700

    Update User request

--- BackEnd/app/Http/Requests/UserRequest.php
+++ BackEnd/app/Http/Requests/UserRequest.php
@@ -11,7 +11,7 @@ class UserRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,35 @@ class UserRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            "name" => "required|string|max:255",
+            "email" => "required|email|max:255",
+            "password" => "required|string|min:8|max:255", 
+            "phone" => "required|string|max:255",
+            "address" => "required|string|max:255",
         ];
+    }
+
+
+    
+       /**
+     * Get the error message for the defined validation rules. 
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            "name.required" => "Name is required.",
+            "name.max" => "Name must not exceed 255 characters.",
+            "email.required" => "Email is required.",
+            "email.max" => "Email must not exceed 255 characters.",
+            "password.required" => "Password is required.",
+            "password.max" => "Password must not exceed 255 characters.",
+            "phone.required" => "Phone number is required.",
+            "phone.max" => "Phone number must not exceed 255 characters.",
+            "address.required" => "Address is required.",
+            "address.max" => "Address must not exceed 255 characters.",
+        ];
+        
     }
 }
