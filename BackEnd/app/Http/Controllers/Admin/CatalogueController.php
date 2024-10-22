ccd146fb8d2174ee27a0f51fbf2c9dafd3a4f973
hoadtph31026 <hoadtph31026.fpt.edu.vn>
2024-10-22 13:53:23 +0700

    Show catalogue

--- BackEnd/app/Http/Controllers/Admin/CatalogueController.php
+++ BackEnd/app/Http/Controllers/Admin/CatalogueController.php
@@ -56,7 +56,8 @@ class CatalogueController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $catalogue = Catalogue::query()->findOrFail($id);
+        return response()->json($catalogue);
     }
 
     /**
