816e55071d78871e871a3adfc7bf0694ceb59438
hoadtph31026 <hoadtph31026.fpt.edu.vn>
2024-10-22 14:12:06 +0700

    Update model product color

--- BackEnd/app/Models/ProductColor.php
+++ BackEnd/app/Models/ProductColor.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ProductColor extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    
+
+    protected $fillable = [
+        "name",
+        "color_code",
+        "status",
+    ];
+
+    protected $casts = [
+        "status" => "boolean",
+    ];
 }
