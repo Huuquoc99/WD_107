ddf8637d1d5367815680844ef12cd6cfa0e06104
hoadtph31026 <hoadtph31026.fpt.edu.vn>
2024-10-24 15:40:42 +0700

    List comment

--- BackEnd/app/Http/Controllers/Admin/CommentController.php
+++ BackEnd/app/Http/Controllers/Admin/CommentController.php
@@ -8,5 +8,9 @@ use App\Http\Controllers\Controller;
 
 class CommentController extends Controller
 {
-
+    public function index()
+    {
+        $comment = Comment::all(); 
+        return response()->json($comment, 200);
+    }
 }
