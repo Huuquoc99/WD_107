698b49a3ccaa9ee065ec64732eba8d5d1ccddc47
hoadtph31026 <hoadtph31026.fpt.edu.vn>
2024-10-24 15:32:34 +0700

    Update model comment

--- BackEnd/app/Models/Comment.php
+++ BackEnd/app/Models/Comment.php
@@ -4,8 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Comment extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'user_id', 
+        'product_id', 
+        'content', 
+        'rating', 
+        'is_active'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
