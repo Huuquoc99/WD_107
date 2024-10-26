9771d89a19a169e8832b0dc85e14bca369d954f3
hoadtph31026 <hoadtph31026.fpt.edu.vn>
2024-10-26 14:11:58 +0700

    List trashed product

--- BackEnd/app/Http/Controllers/Admin/TrashedController.php
+++ BackEnd/app/Http/Controllers/Admin/TrashedController.php
@@ -2,10 +2,15 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
+use App\Models\Product;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class TrashedController extends Controller
 {
-    //
+    public function trashed()
+    {
+        $trashed = Product::onlyTrashed()->get();
+        return response()->json($trashed);
+    }
 }
--- BackEnd/routes/api.php
+++ BackEnd/routes/api.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\Admin\BannerController;
 use App\Http\Controllers\Admin\CommentController;
 use App\Http\Controllers\Admin\CatalogueController;
+use App\Http\Controllers\Admin\TrashedController;
 use App\Http\Controllers\Client\ClientUserController;
 use App\Http\Controllers\Client\CommentController as ClientCommentController;
 
@@ -48,6 +49,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
         Route::get('/admin/comments', [CommentController::class, 'index']); 
         Route::put('/admin/comments/approve/{id}', [CommentController::class, 'approve']); 
         Route::delete('/admin/comments/{id}', [CommentController::class, 'destroy']); 
+
+        // Trash
+        Route::get('/admin/trashed', [TrashedController::class, 'trashed']);
     });
 
 
