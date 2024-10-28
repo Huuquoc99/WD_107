f1e997a8eec632fd8aa481f86985c022ac73a199
hoadtph31026 <hoadtph31026.fpt.edu.vn>
2024-10-28 15:55:49 +0700

    Create checkout controller

---
+++ BackEnd/app/Http/Controllers/Client/CheckoutController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Client;
+
+use App\Models\OrderItem;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+
+class CheckoutController extends Controller
+{
+    
+}
--- BackEnd/routes/api.php
+++ BackEnd/routes/api.php
@@ -1,22 +1,23 @@
 <?php
-use App\Http\Controllers\Admin\ProductController;
-use App\Http\Controllers\Client\CartControler;
+use App\Models\ProductColor;
 use Illuminate\Http\Request;
 use App\Models\ProductCapacity;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\Client\CartControler;
+use App\Http\Controllers\Client\HomeController;
 use App\Http\Controllers\Client\ShopController;
 use App\Http\Controllers\Admin\BannerController;
 use App\Http\Controllers\Admin\CommentController;
+use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\Admin\TrashedController;
 use App\Http\Controllers\Admin\CatalogueController;
+use App\Http\Controllers\Client\CheckoutController;
 use App\Http\Controllers\Admin\StatusOrderController;
-use App\Http\Controllers\Admin\StatusPaymentController;
 use App\Http\Controllers\Client\ClientUserController;
+use App\Http\Controllers\Admin\StatusPaymentController;
 use App\Http\Controllers\Client\CommentController as ClientCommentController;
-use App\Http\Controllers\Client\HomeController;
-use App\Models\ProductColor;
 
 /*
 |--------------------------------------------------------------------------
@@ -88,3 +89,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
     // Cart
     Route::post('/product/add-to-cart', [CartControler::class, 'addToCart']);
+
+    // Checkout
+    Route::post('/checkout', [CheckoutController::class, 'checkout']);
