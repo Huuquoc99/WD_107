dbadf9aa97efe6d7c0f54067c3311653327749fc
hoadtph31026 <hoadtph31026.fpt.edu.vn>
2024-10-28 10:22:38 +0700

    Store status order

--- BackEnd/app/Http/Controllers/Admin/StatusOrderController.php
+++ BackEnd/app/Http/Controllers/Admin/StatusOrderController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StatusOrderRequest;
 use App\Models\StatusOrder;
 use Illuminate\Http\Request;
 
@@ -28,9 +29,15 @@ class StatusOrderController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StatusOrderRequest $request)
     {
-        //
+        if ($request->isMethod("POST")) {
+            $param = $request->except("_token",);
+        
+            StatusOrder::create($param);
+        
+            return response()->json(['message' => 'Status order created successfully']);
+        }
     }
 
     /**
